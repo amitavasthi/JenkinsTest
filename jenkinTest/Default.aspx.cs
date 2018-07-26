66e85b62516c4f697c4dc7be144e47669275cdc0
amitavasthi <amit.a@blueoceanmi.com>
2018-07-26 13:10:41 +0530

    Test Jenkin

---
+++ jenkinTest/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace jenkinTest
+{
+    public partial class _Default : Page
+    {
+        public string globalmsg = "CI 1";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
